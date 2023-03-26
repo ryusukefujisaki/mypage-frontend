fa601fa18b0feb12cb5b0a0ef3af656adea6a12e
r_fujisaki <ryusukefujisaki@gmail.com>
2023-03-26 10:14:18 +0900

    Add backend framework page

--- src/pages/backend_framework/BackendFramework.vue
+++ src/pages/backend_framework/BackendFramework.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+import axios from '@/utilities/axios'
+
+axios.get('/').then(response => {
+  document.getElementsByClassName('backend-framework')[0].innerHTML = response.data
+})
+</script>
+
 <template>
-  backend_framework
+  <div class="backend-framework"></div>
 </template>
