e5aab5ebec9b881be0aea2874410158b2e72d712
r_fujisaki <ryusukefujisaki@gmail.com>
2023-03-29 05:36:42 +0900

    Add article list

--- src/pages/article/ArticleList.vue
+++ src/pages/article/ArticleList.vue
@@ -1,3 +1,29 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import axios from '@/utilities/axios'
+
+interface Article { title: string, created_at: string }
+const articles = ref<Article[]>([])
+  axios.get('/articles').then(response => {
+  articles.value = response.data
+})
+</script>
+
 <template>
-  article_list
+  <table class="table is-striped is-hoverable is-fullwidth">
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Title</th>
+        <th>Created At</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(article, index) in articles" :key="index">
+        <th>{{ index + 1 }}</th>
+        <td>{{ article.title }}</td>
+        <td>{{ article.created_at }}</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
