a5f5b119d1f4bbff6291d50ef46a106b0cdcae8b
r_fujisaki <ryusukefujisaki@gmail.com>
2023-04-02 18:18:45 +0900

    Add article detail

--- src/pages/article/ArticleDetail.vue
+++ src/pages/article/ArticleDetail.vue
@@ -1,4 +1,22 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useRoute } from 'vue-router'
+import axios from '@/utilities/axios'
+import { formatDate } from '@/utilities/displayHelper'
+
+const id = useRoute().params.id
+interface Article { title: string, content: string, created_at: string, updated_at: string }
+const articleDetail = ref<Article>({})
+axios.get(`/articles/${id}`).then(response => {
+  articleDetail.value = response.data
+})
+</script>
+
 <template>
-  article_detail
-  {{ $route.params.id }}
+  <p class="title">{{ articleDetail.title }}</p>
+  <hr>
+  <div>{{ articleDetail.content }}</div>
+  <br>
+  <p>Created At: {{ formatDate(articleDetail.created_at) }}</p>
+  <p>Updated At: {{ formatDate(articleDetail.updated_at) }}</p>
 </template>
