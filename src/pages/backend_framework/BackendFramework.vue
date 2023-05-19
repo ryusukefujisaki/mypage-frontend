226c3307c6791bf2270300bd5f650a7afde3dd60
r_fujisaki <ryusukefujisaki@gmail.com>
2023-05-20 00:25:54 +0900

    Fix crazy variable name

--- src/pages/backend_framework/BackendFramework.vue
+++ src/pages/backend_framework/BackendFramework.vue
@@ -3,11 +3,11 @@ import Header from '@/components/Header.vue'
 import axios from '@/utilities/axios'
 
 axios.get('/').then(response => {
-  const backgroundFrameworkData = response.data
+  const backendFrameworkData = response.data
     .replace(/<link rel="icon" href=.+>/, '')
     .replace('body {', '.backend-framework body .{')
     .replace('ul {', '.backend-framework ul .{')
-  document.getElementsByClassName('backend-framework')[0].innerHTML = backgroundFrameworkData
+  document.getElementsByClassName('backend-framework')[0].innerHTML = backendFrameworkData
 })
 </script>
 
